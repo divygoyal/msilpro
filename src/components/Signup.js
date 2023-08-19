18a2874b3928139af4a9d9e8efffa2187be6d5f0
Divy Goyal <86590133+divygoyal@users.noreply.github.com>
2023-08-19 19:28:14 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -55,13 +55,13 @@ const Signup = () => {
         
           <h1 className='logintext'>Register</h1>
           <div className="emailbox">
-              <label  for="name" >Email</label>
+              <label  for="name"></label>
             <input type="text" name="name" id="name" placeholder="Your Name"  
                                     value = {user.name} onChange={helper}  style={{fontFamily:"Outfit"}}  />
             
             {/* <svg style={{height:"30px" , width:"30px" , color:"white"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="Email"><path fill="#ffffff" d="M53.42 53.32H10.58a8.51 8.51 0 0 1-8.5-8.5V19.18a8.51 8.51 0 0 1 8.5-8.5h42.84a8.51 8.51 0 0 1 8.5 8.5v25.64a8.51 8.51 0 0 1-8.5 8.5ZM10.58 13.68a5.5 5.5 0 0 0-5.5 5.5v25.64a5.5 5.5 0 0 0 5.5 5.5h42.84a5.5 5.5 0 0 0 5.5-5.5V19.18a5.5 5.5 0 0 0-5.5-5.5Z" class="color222222 svgShape"></path><path fill="#ffffff" d="M32 38.08a8.51 8.51 0 0 1-5.13-1.71L3.52 18.71a1.5 1.5 0 1 1 1.81-2.39L28.68 34a5.55 5.55 0 0 0 6.64 0l23.35-17.68a1.5 1.5 0 1 1 1.81 2.39L37.13 36.37A8.51 8.51 0 0 1 32 38.08Z" class="color222222 svgShape"></path><path fill="#ffffff" d="M4.17 49.14a1.5 1.5 0 0 1-1-2.62l18.4-16.41a1.5 1.5 0 0 1 2 2.24L5.17 48.76a1.46 1.46 0 0 1-1 .38zm55.66 0a1.46 1.46 0 0 1-1-.38l-18.4-16.41a1.5 1.5 0 1 1 2-2.24l18.39 16.41a1.5 1.5 0 0 1-1 2.62z" class="color222222 svgShape"></path></svg> */}
           </div>
-          <div className="passwordbox">
+          <div className="emailbox">
           <label for="email"></label>
                                 <input type="email" name="email" id="email" placeholder="Your Email"  
                                     value = {user.email} onChange={helper}  style={{fontFamily:"Outfit"}}
@@ -127,7 +127,7 @@ const Signup = () => {
           
           <button id="submitbtn" style={{fontFamily:"Outfit"}} type="submit" name="signup" value="Register" onClick={postdata}>Login</button>
 
-          <p onClick={()=>{
+          <p style={{color:"white"}}onClick={()=>{
                             history.push("/Login");
                         }} >Have an account?<a style={{fontWeight:"600" , color:'white',textDecoration:"none"}}> Login</a></p>
        
